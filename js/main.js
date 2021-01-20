ba739091be2d00d13b79ba784a85eb84f88a7601
sheitannn <55089950+sheitannn@users.noreply.github.com>
2021-01-21 00:59:26 +0100

    noo more pages different typo landing page
    new

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function openNav() {
-  document.getElementById("mySidenav").style.height = "100%";
-}
-
-/* Close/hide the sidenav */
-function closeNav() {
-  document.getElementById("mySidenav").style.height = "0";
-}
